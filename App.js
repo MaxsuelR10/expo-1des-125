fc3bd951b5856183bc8371a91ecf3e0ccc2e346c
MAXSUEL RODRIGUES SANTOS <maxsuelrodriguessantos15@gmail.com>
2026-02-25 20:37:56 -0300

    aula 25/02

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native'; 
+import { StyleSheet, View } from 'react-native';
 import { RFPercentage } from "react-native-responsive-fontsize";
 import Constants from 'expo-constants';
 
+import { NavigationContainer } from '@react-navigation/native';
+
+import StackExemplos from './src/navegacao/stackExemplos';
+
 import Exemplo01 from './src/exemplos/ex01';
-import Exemplo02 from './src/exemplos/ex02'; 
-import Exemplo03 from './src/exemplos/ex03'; 
-import Exemplo04 from './src/exemplos/ex04'; 
-import Exemplo05 from './src/exemplos/ex05'; 
+import Exemplo02 from './src/exemplos/ex02';
+import Exemplo03 from './src/exemplos/ex03';
+import Exemplo04 from './src/exemplos/ex04';
+import Exemplo05 from './src/exemplos/ex05';
 import Exemplo06 from './src/exemplos/ex06';
 import Exemplo07 from './src/exemplos/ex07';
 import Exemplo08 from './src/exemplos/ex08';
 import Exemplo09 from './src/exemplos/ex09';
 
+import Exemplos from './src/exemplos/listaExemplos';
+
+
 import Atividade01 from './src/atividades/atv01';
-import Atividade02 from './src/atividades/atv02'; 
+import Atividade02 from './src/atividades/atv02';
 import Atividade03 from './src/atividades/atv03';
-import Atividade04 from './src/atividades/atv04'; 
-import Atividade05 from './src/atividades/atv05'; 
+import Atividade04 from './src/atividades/atv04';
+import Atividade05 from './src/atividades/atv05';
 import Atividade06 from './src/atividades/atv06';
 import Atividade07 from './src/atividades/atv07';
 
@@ -31,7 +38,9 @@ import Navegacao from './src/revisao/navegacao';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Navegacao />
+      <NavigationContainer>
+        <StackExemplos />
+      </NavigationContainer>
       <StatusBar style="light" />
     </View>
   );
@@ -41,8 +50,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'darkslategrey',
-    padding: RFPercentage(1.2), 
-    paddingTop: Constants.statusBarHeight, 
+    padding: RFPercentage(1.2),
+    paddingTop: Constants.statusBarHeight,
   },
 });
 
